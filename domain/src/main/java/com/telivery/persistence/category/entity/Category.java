1e7634328e04e72a9a3c996d1c5358ade310c578
EunjiShin <eunji980310@gmail.com>
2022-05-14 22:51:33 +0900

    :card_file_box: Category Entity 추가

--- domain/src/main/java/com/telivery/persistence/category/entity/Category.java
+++ domain/src/main/java/com/telivery/persistence/category/entity/Category.java
@@ -1,12 +1,14 @@
 package com.telivery.persistence.category.entity;
 
 import com.telivery.common.domain.BaseEntity;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,5 +21,13 @@ public class Category extends BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(length = 50)
+  private String name;
+
+  @Builder
+  public Category(String name) {
+    this.name = name;
+  }
 
 }
