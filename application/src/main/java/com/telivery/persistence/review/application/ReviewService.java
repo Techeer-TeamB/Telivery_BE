870cd4a943bf63654fd8a743f00b4fad98df9fe4
EunjiShin <eunji980310@gmail.com>
2022-05-28 21:56:21 +0900

    :truck: review, time application 패키지 수정

--- application/src/main/java/com/telivery/persistence/review/application/ReviewService.java
+++ application/src/main/java/com/telivery/persistence/review/application/ReviewService.java
@@ -1,19 +1,19 @@
-package com.telivery.persistence.time;
+package com.telivery.persistence.review.application;
 
-import com.telivery.persistence.time.dao.TimeRepository;
+import com.telivery.persistence.review.dao.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class TimeService {
+public class ReviewService {
 
-  private final TimeRepository timeRepository;
+  private final ReviewRepository reviewRepository;
 
   @Transactional(readOnly = true)
   public long countByRestaurantId(Long restaurantId) {
-    return timeRepository.countByRestaurantId(restaurantId);
+    return reviewRepository.countByRestaurantId(restaurantId);
   }
 
 }
