78be50165f5ae6dfd29a25b4e76288b0c869961c
EunjiShin <eunji980310@gmail.com>
2022-05-28 22:36:36 +0900

    :sparkles: restaurant 상세 정보 조회 API 추가

--- api/src/main/java/com/telivery/controller/restaurant/RestaurantController.java
+++ api/src/main/java/com/telivery/controller/restaurant/RestaurantController.java
@@ -1,7 +1,28 @@
 package com.telivery.controller.restaurant;
 
-import org.springframework.stereotype.Controller;
+import com.telivery.persistence.restaurant.application.RestaurantService;
+import com.telivery.persistence.restaurant.dto.RestaurantDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/restaurants")
 public class RestaurantController {
+
+  private final RestaurantService restaurantService;
+
+  @GetMapping("/{restaurantId}")
+  public ResponseEntity<RestaurantDTO> getRestaurantInfo(
+      @PathVariable final Long restaurantId
+  ) {
+    return new ResponseEntity<>(restaurantService.getRestaurantInfo(restaurantId), HttpStatus.OK);
+  }
 }
--- application/src/main/java/com/telivery/persistence/restaurant/application/RestaurantService.java
+++ application/src/main/java/com/telivery/persistence/restaurant/application/RestaurantService.java
@@ -1,6 +1,12 @@
 package com.telivery.persistence.restaurant.application;
 
+import com.telivery.common.exception.global.NoDataException;
 import com.telivery.persistence.restaurant.dao.RestaurantRepository;
+import com.telivery.persistence.restaurant.dto.RestaurantDTO;
+import com.telivery.persistence.restaurant.entity.Restaurant;
+import com.telivery.persistence.review.application.ReviewService;
+import com.telivery.persistence.time.application.TimeService;
+import com.telivery.persistence.time.dto.TimeDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +14,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RestaurantService {
 
+  private final TimeService timeService;
+  private final ReviewService reviewService;
   private final RestaurantRepository restaurantRepository;
 
+  public RestaurantDTO getRestaurantInfo(Long restaurantId) {
+    Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(
+        NoDataException::new);
+    TimeDTO workHour = timeService.findOneByRestaurantId(restaurantId);
+    Long reviewCount = reviewService.countByRestaurantId(restaurantId);
+
+    return new RestaurantDTO(restaurant, workHour, reviewCount);
+  }
 
 }
