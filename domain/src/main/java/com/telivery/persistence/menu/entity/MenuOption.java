cb15ab8f2a3b35d02e48186d33b3369bb84c4f4d
EunjiShin <eunji980310@gmail.com>
2022-07-02 14:58:24 +0900

    :card_file_box: menu_option entity 생성

---
+++ domain/src/main/java/com/telivery/persistence/menu/entity/MenuOption.java
@@ -0,0 +1,43 @@
+package com.telivery.persistence.menu.entity;
+
+import com.sun.istack.NotNull;
+import com.telivery.common.domain.BaseEntity;
+import com.telivery.persistence.category.entity.Category;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "menu_option")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MenuOption extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "menu_id")
+  private Menu menu;
+
+  @NotNull
+  @Column(name = "is_essential")
+  private boolean isEssential;
+
+  @Column(length = 50)
+  private String name;
+
+  @NotNull
+  private int price;
+
+}
