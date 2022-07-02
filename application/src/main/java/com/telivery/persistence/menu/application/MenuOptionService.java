f3efe1d684876415d40b21f1d6840eafb91a05be
EunjiShin <eunji980310@gmail.com>
2022-07-02 16:26:41 +0900

    :sparkles: 메뉴별 메뉴 옵션 리스트 조회 API 추가

--- application/src/main/java/com/telivery/persistence/menu/application/MenuOptionService.java
+++ application/src/main/java/com/telivery/persistence/menu/application/MenuOptionService.java
@@ -1,9 +1,11 @@
 package com.telivery.persistence.menu.application;
 
 import com.telivery.persistence.menu.dao.MenuOptionRepository;
+import com.telivery.persistence.menu.dto.MenuOptionDTO.MenuOptionRes;
 import com.telivery.persistence.menu.entity.MenuOption;
 import com.telivery.persistence.menu.exception.NoOptionInMenuException;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,11 @@ public class MenuOptionService {
     List<MenuOption> menuOptionList = menuOptionRepository.findAllByMenu(menuId);
     if (menuOptionList.isEmpty()) throw new NoOptionInMenuException();
     return menuOptionList;
+  }
+
+  public List<MenuOptionRes> findResByMenu(long menuId) {
+    List<MenuOption> menuOptionList = findByMenu(menuId);
+    return menuOptionList.stream().map(MenuOptionRes::new).collect(Collectors.toList());
   }
 
 }
