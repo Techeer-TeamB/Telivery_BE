1c1c3fcb27106a8343197101d57bffeefe9a6ec6
EunjiShin <eunji980310@gmail.com>
2022-07-02 14:50:31 +0900

    :card_file_box: menu entity 생성

---
+++ domain/src/main/java/com/telivery/persistence/menu/entity/Menu.java
@@ -0,0 +1,56 @@
+package com.telivery.persistence.menu.entity;
+
+import com.telivery.common.domain.BaseEntity;
+import com.telivery.persistence.category.entity.Category;
+import com.telivery.persistence.restaurant.entity.Restaurant;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "menu")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Menu extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "restaurant_id")
+  private Restaurant restaurant;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id")
+  private Category category;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "sub_category_id")
+  private Category subCategory;
+
+  @Column(length = 50)
+  private String name;
+
+  private int price;
+
+  @Column(length = 255)
+  private String description;
+
+  @Column(length = 255)
+  private String photo;
+
+  @Column(length = 30)
+  private String status;
+
+}
