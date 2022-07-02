8bbac13f830e3afd07679b830532918d387ad1a9
EunjiShin <eunji980310@gmail.com>
2022-07-02 15:54:35 +0900

    :sparkles: 메뉴 단일 조회 & 가게별 메뉴 조회 메소드 추가

--- application/src/main/java/com/telivery/persistence/menu/application/MenuService.java
+++ application/src/main/java/com/telivery/persistence/menu/application/MenuService.java
@@ -2,6 +2,9 @@ package com.telivery.persistence.menu.application;
 
 import com.telivery.persistence.menu.dao.MenuRepository;
 import com.telivery.persistence.menu.entity.Menu;
+import com.telivery.persistence.menu.exception.MenuIdNotFoundException;
+import com.telivery.persistence.menu.exception.NoMenuInRestaurantException;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +15,15 @@ public class MenuService {
 
   private final MenuRepository menuRepository;
 
-  public Menu findById (long id) {
-    Optional<Menu> menu = menuRepository.findById(id);
+  public Menu findById(long id) {
+    Menu menu = menuRepository.findById(id).orElseThrow(MenuIdNotFoundException::new);
+    return menu;
+  }
+
+  public List<Menu> findByRestaurant(long restaurantId) {
+    List<Menu> menuList = menuRepository.findByRestaurant(restaurantId);
+    if (menuList.isEmpty()) throw new NoMenuInRestaurantException();
+    return menuList;
   }
 
 }
--- domain/src/main/java/com/telivery/persistence/menu/dao/MenuRepository.java
+++ domain/src/main/java/com/telivery/persistence/menu/dao/MenuRepository.java
@@ -2,10 +2,13 @@ package com.telivery.persistence.menu.dao;
 
 import com.telivery.persistence.menu.entity.Menu;
 import com.telivery.persistence.restaurant.entity.Restaurant;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface MenuRepository extends PagingAndSortingRepository<Menu, Long>,
     JpaRepository<Menu, Long> {
+
+  List<Menu> findByRestaurant(long restaurantId);
 
 }
