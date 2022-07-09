641ad9b6e468a6d58ceba55267d101a5deabc795
EunjiShin <eunji980310@gmail.com>
2022-07-09 15:11:23 +0900

    :card_file_box: user entity 1차 설정 완료

--- domain/src/main/java/com/telivery/persistence/user/entity/User.java
+++ domain/src/main/java/com/telivery/persistence/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.telivery.persistence.user.entity;
 
+import com.telivery.common.domain.BaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,9 +12,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Entity
 @Table(name = "user")
-public class User {
+public class User extends BaseEntity {
     @Id
     @GeneratedValue
     @Column(name="id")
     private Long id;
+
+    @Column(length = 30, unique = true, nullable = false)
+    private String username;
+
+    @Column(length = 13)
+    private String phone;
+
+    @Column()
+    private String email;
+
+    @Column()
+    private String address;
+
+    @Column(length = 20, name = "class")
+    private String gradle;
+
+    private Integer point;
+
+    private Float lat;
+
+    private Float lng;
+
 }
