53ceeb9e931ac8815fc303028a03192e449c0778
EunjiShin <eunji980310@gmail.com>
2022-07-09 15:04:08 +0900

    :fire: 사용하지 않는 코드 삭제

--- domain/src/main/java/com/telivery/persistence/user/entity/User.java
+++ domain/src/main/java/com/telivery/persistence/user/entity/User.java
@@ -1,45 +1,19 @@
 package com.telivery.persistence.user.entity;
 
-import com.telivery.persistence.user.entity.type.Role;
-import lombok.*;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.Builder;
+import lombok.Getter;
 
 import javax.persistence.*;
-import java.util.Date;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@Setter
 @Builder
 @RequiredArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue
     @Column(name="id")
     private Long id;
-
-    @CreatedDate
-    @Column(name="createdAt")
-    private Date createdAt;
-
-    @UpdateTimestamp
-    @Column(name="updatedAt")
-    private Date updatedAt;
-
-    @Column(name="email")
-    private String email;
-
-    @Column(name="password")
-    private String password;
-
-    @Column(name = "username")
-    private String username;
-
-    @Column(name = "birthday")
-    private Date birthday;
-
-    @Column(name="role")
-    private Role role;
 }
--- domain/src/main/java/com/telivery/persistence/user/entity/type/Role.java
+++
@@ -1,5 +0,0 @@
-package com.telivery.persistence.user.entity.type;
-
-public enum Role {
-    USER, ADMIN
-}
