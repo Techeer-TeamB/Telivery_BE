5d4e1be6a9b4626291ac4580a157249a18e7bca8
EunjiShin <eunji980310@gmail.com>
2022-07-09 14:46:23 +0900

    :sparkles: jwt 권한 관련 exception handler 설정 추가

--- domain/src/main/java/com/telivery/common/exception/ErrorCode.java
+++ domain/src/main/java/com/telivery/common/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     /* User */
     USER_NOT_FOUND(-100, "존재하지 않는 user 입니다"),
     TOKEN_NOT_FOUND(-101, "토큰이 존재하지 않습니다."),
-    EXPIRED_TOKEN(-102, "만료된 토큰입니다.");
+    EXPIRED_TOKEN(-102, "만료된 토큰입니다."),
+    ACCOUNT_ACCESS_DENIED(-103, "사용자에게 필요 권한이 존재하지 않습니다.");
 
 
     private final int code;
--- domain/src/main/java/com/telivery/common/security/jwt/JwtAccessDeniedHandler.java
+++ domain/src/main/java/com/telivery/common/security/jwt/JwtAccessDeniedHandler.java
@@ -1,5 +1,7 @@
 package com.telivery.common.security.jwt;
 
+import com.google.gson.Gson;
+import com.telivery.common.exception.ErrorCode;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +17,15 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
   @Override
   public void handle(HttpServletRequest request, HttpServletResponse response,
       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    setResponse(response, ErrorCode.ACCOUNT_ACCESS_DENIED);
+  }
+
+  private void setResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+    response.setContentType("application/json;");
+    response.setCharacterEncoding("UTF-8");
+    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    JwtExceptionResponse jwtExceptionResponse = new JwtExceptionResponse(errorCode.getCode(), errorCode.getMessage());
+    response.getWriter().write(new Gson().toJson(jwtExceptionResponse));
   }
 
 }
