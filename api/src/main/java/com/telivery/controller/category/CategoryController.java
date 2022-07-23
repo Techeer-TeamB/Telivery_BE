eb4a7c34c23ea22485f0ae671e0f4f12a796262d
Eunji-gec <ejsin@gentleenergycorp.com>
2022-07-24 03:21:48 +0900

    :bulb: CurrentUser 커스텀 어노테이션 사용 예시 추가

--- api/src/main/java/com/telivery/controller/category/CategoryController.java
+++ api/src/main/java/com/telivery/controller/category/CategoryController.java
@@ -25,6 +25,7 @@ public class CategoryController {
   @GetMapping
   public ResponseEntity<List<CategoryExampleDto>> findAll(@CurrentUser User user) {
     System.out.println("로그인 유저 : "+user.getUsername());
+    System.out.println("로그인 유저 PK : "+user.getId());
     List<CategoryExampleDto> categoryList = categoryService.findAll();
     return new ResponseEntity<>(categoryList, HttpStatus.OK);
   }
