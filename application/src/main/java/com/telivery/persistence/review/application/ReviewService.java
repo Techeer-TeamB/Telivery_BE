83eda653725b1897a58098e3e5870eed342d3256
EunjiShin <eunji980310@gmail.com>
2022-07-24 23:01:11 +0900

    :sparkles: 리뷰 response DTO 생성 코드 추가

--- application/src/main/java/com/telivery/persistence/review/application/ReviewService.java
+++ application/src/main/java/com/telivery/persistence/review/application/ReviewService.java
@@ -1,7 +1,9 @@
 package com.telivery.persistence.review.application;
 
 import com.telivery.persistence.order.application.OrderService;
+import com.telivery.persistence.order.dao.OrderMenuRepository;
 import com.telivery.persistence.order.entity.Order;
+import com.telivery.persistence.order.entity.OrderMenu;
 import com.telivery.persistence.restaurant.application.RestaurantService;
 import com.telivery.persistence.restaurant.entity.Restaurant;
 import com.telivery.persistence.review.dao.ReviewRepository;
@@ -9,6 +11,9 @@ import com.telivery.persistence.review.dto.ReviewDTO.ReviewReq;
 import com.telivery.persistence.review.dto.ReviewDTO.ReviewRes;
 import com.telivery.persistence.review.entity.Review;
 import com.telivery.persistence.user.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewService {
 
   private final ReviewRepository reviewRepository;
+  private final OrderMenuRepository orderMenuRepository;
   private final RestaurantService restaurantService;
   private final OrderService orderService;
 
@@ -28,19 +34,21 @@ public class ReviewService {
 
   @Transactional(readOnly = true)
   public ReviewRes create(User user, long restaurantId, long orderId, ReviewReq reviewReq) {
+    // DESCRIBE: 리뷰 생성
     Restaurant restaurant = restaurantService.findById(restaurantId);
     Order order = orderService.findByIdAndUser(orderId, user.getId());
     Review review = reviewReq.toEntity(user, restaurant, order);
-
-    // DESCRIBE: 리뷰 생성
     reviewRepository.save(review);
 
     // DESCRIBE: 생성한 리뷰의 평점을 이용, 가게 평점 업데이트
 
-    // DESCRIBE: 주문 메뉴 리스트 생성
+    // DESCRIBE: 주문 메뉴 리스트 확인
+    List<String> menuList = new ArrayList<>();
+    List<OrderMenu> orderMenus = orderMenuRepository.findByOrderId(orderId);
+    if (!orderMenus.isEmpty()) menuList = orderMenus.stream().map(OrderMenu::getName).collect(
+        Collectors.toList());
 
-
-    return new ReviewRes(review);
+    return new ReviewRes(review, menuList);
   }
 
 }
