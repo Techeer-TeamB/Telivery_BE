21b9686168db72d93feccf4b05ee346117be37ea
EunjiShin <eunji980310@gmail.com>
2022-07-24 22:21:25 +0900

    :sparkles: review controller 추가

---
+++ api/src/main/java/com/telivery/controller/review/ReviewController.java
@@ -0,0 +1,37 @@
+package com.telivery.controller.review;
+
+import com.telivery.controller.user.CurrentUser;
+import com.telivery.persistence.review.application.ReviewService;
+import com.telivery.persistence.review.dto.ReviewDTO.ReviewReq;
+import com.telivery.persistence.review.dto.ReviewDTO.ReviewRes;
+import com.telivery.persistence.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class ReviewController {
+
+  private final ReviewService reviewService;
+
+  @PostMapping(value = "/restaurants/{restaurantId}/orders/{orderId}/review")
+  public ResponseEntity<ReviewRes> create(
+      @CurrentUser final User user,
+      @PathVariable final long restaurantId,
+      @PathVariable final long orderId,
+      @RequestBody final ReviewReq reviewReq
+  ) {
+    return new ResponseEntity<>(reviewService.create(user, restaurantId, orderId, reviewReq), HttpStatus.OK);
+  }
+
+}
--- application/src/main/java/com/telivery/persistence/review/application/ReviewService.java
+++ application/src/main/java/com/telivery/persistence/review/application/ReviewService.java
@@ -1,6 +1,9 @@
 package com.telivery.persistence.review.application;
 
 import com.telivery.persistence.review.dao.ReviewRepository;
+import com.telivery.persistence.review.dto.ReviewDTO.ReviewReq;
+import com.telivery.persistence.review.dto.ReviewDTO.ReviewRes;
+import com.telivery.persistence.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +17,11 @@ public class ReviewService {
   @Transactional(readOnly = true)
   public long countByRestaurantId(Long restaurantId) {
     return reviewRepository.countByRestaurantId(restaurantId);
+  }
+
+  @Transactional(readOnly = true)
+  public ReviewRes create(User user, long restaurantId, long orderId, ReviewReq reviewReq) {
+
   }
 
 }
